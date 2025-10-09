26477dd4dfeca294c2236e11e03c1bdd12f5a932
annejulia milian <amilian@ucsd.edu>
2025-10-09 12:16:17 -0700

    AJ: added music.ts

---
+++ src/music.ts
@@ -0,0 +1,19 @@
+const Playlist: string[] = [
+    "Song 1- Artist A",
+    "Song 2- Artist B",
+    "Song 3- Artist C",
+    "Song 4- Artist D",
+    "Song 5- Artist E"
+];
+
+
+function printPlaylist(Playlist: string[]) {
+    console.log("Playlist: " );
+    for(let i = 0; i < Playlist.length; i++) {
+        console.log("♡ " + Playlist[i]);
+    }
+}
+
+
+printPlaylist(Playlist);
+
