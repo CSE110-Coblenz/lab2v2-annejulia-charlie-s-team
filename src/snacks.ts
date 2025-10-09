2efb9c24870261846fd3c7ba199189528ad78961
Charlie Suarez Robles <charlie.suarez.robles@gmail.com>
2025-10-09 11:23:08 -0700

    Created a new source file

---
+++ src/snacks.ts
@@ -0,0 +1,11 @@
+let snackArray: string[] = ["chips", "fruit", "crackers", "yogurt", "granola bar"]
+
+export function printAllSnacks(): void {
+    console.log(snackArray)
+}
+
+function main (): void {
+    printAllSnacks()
+}
+
+main();
