de790e30abb38bfd5e61b9a5be1bf46d3c25ef67
annejulia milian <amilian@ucsd.edu>
2025-10-09 11:48:55 -0700

    Aj: added snacks.ts

---
+++ src/snacks.ts
@@ -0,0 +1,18 @@
+const snacks: string[] = [
+    "Carrots and Hummus",
+    "Mango",
+    "Rice Cake",
+    "Chips"
+];
+
+
+function printSnacksArray(snacks: string[]) {
+    console.log("Snacks Array: " );
+    for(let i = 0; i < snacks.length; i++) {
+        console.log("♡ " + snacks[i]);
+    }
+}
+
+
+printSnacksArray(snacks);
+
