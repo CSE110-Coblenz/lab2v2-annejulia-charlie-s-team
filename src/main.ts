8093096d85121ae76cdacee5f13106b3432ea20a
annejulia milian <amilian@ucsd.edu>
2025-10-09 11:55:10 -0700

    Aj: added main.ts to print snacks

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { printSnacksArray } from "./snacks";
+
+function main() {
+  console.log("૮꒰ ˶• ༝ •˶꒱ა ♡ Annejulia's Snack List ♡");
+  printSnacksArray([
+    "Carrots and Hummus",
+    "Mango",
+    "Rice Cake",
+    "Chips"
+  ]);
+}
+
+main();
