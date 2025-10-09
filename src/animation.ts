5245b94ba8e371e74c81c47870c14da79d7a1378
annejulia milian <amilian@ucsd.edu>
2025-10-09 12:44:44 -0700

    added animation.ts

---
+++ src/animation.ts
@@ -0,0 +1,7 @@
+export function animateFeature(featureName: string): void {
+  const bold = "\x1b[1m";     // ANSI code for bold
+  const italic = "\x1b[3m";   // ANSI code for italics
+  const reset = "\x1b[0m";    // reset text style
+
+  console.log(`${bold}Party! Party! Party! - ${italic}${featureName}${reset}`);
+}
